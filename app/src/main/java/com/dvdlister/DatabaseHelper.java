78622aba098bde7808df0a5eafad952b2f2f95a3
jplabadie <jhl42@nau.edu>
2017-10-06 19:05:50 -0700

    Add new tables for meta data to the database

--- app/src/main/java/com/dvdlister/DatabaseHelper.java
+++ app/src/main/java/com/dvdlister/DatabaseHelper.java
@@ -26,15 +26,34 @@ import java.io.FileWriter;
 class DatabaseHelper extends SQLiteOpenHelper {
 
     private static final String DB_NAME = "qrcodes.db";
-    private static final String TBL_DVD_MAIN = "dvd_tbl";
-    private static final String TBL_DVD_CREDITS = "dvd_credits";
-    private static final String TBL_DVD_KEYWORDS = "dvd_keywords";
-    private static final String TBL_DVD_DETAILS = "dvd_details";
-    public static final String COL_DVD_ID = "id";
+
+    private static final String TBL_DVD = "dvd_tbl";
     private static final String COL_DVD_QRCODE = "qrcode";
     private static final String COL_DVD_TITLE = "title";
     public static final String COL_DVD_DESCRIPTION = "description";
+    public static final String COL_DVD_OVERVIEW = "overview";
     private static final String COL_DVD_LOCATION = "location";
+
+    private static final String TBL_CREDITS = "dvd_credits";
+    private static final String COL_NAME = "name";
+    private static final String COL_ROLE = "role";
+
+    private static final String TBL_KEYWORDS = "dvd_keywords";
+    private static final String COL_KEYWORD = "keyword";
+
+    private static final String TBL_GENRE = "dvd_genre";
+    private static final String COL_GENRE = "genre";
+    private static final String COL_DESCRIPTION = "description";
+
+    private static final String TBL_LOCATION = "dvd_location";
+    private static final String COL_LOCATION = "location";
+
+    private static final String TBL_DVD_KEYWORDS = "dvd_to_genre";
+    private static final String TBL_DVD_CREDITS = "dvd_to_credits";
+    private static final String TBL_DVD_GENRE = "dvd_to_genre";
+    private static final String TBL_DVD_LOCATION = "dvd_to_location";
+
+
 
 
     DatabaseHelper(Context context) {
@@ -43,19 +62,52 @@ class DatabaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL( "CREATE TABLE " + TBL_DVD_MAIN + "(QRCODE TEXT PRIMARY KEY," +
-                "TITLE TEXT,DESCRIPTION TEXT,OVERVIEW TEXT,GENRE TEXT,LOCATION TEXT)");
-        db.execSQL( "CREATE TABLE " + TBL_DVD_KEYWORDS + "(QRCODE TEXT PRIMARY KEY," +
-                "TITLE TEXT,DESCRIPTION TEXT,OVERVIEW TEXT,GENRE TEXT,LOCATION TEXT)");
-        db.execSQL( "CREATE TABLE " + TBL_DVD_CREDITS + "(QRCODE TEXT PRIMARY KEY," +
-                "TITLE TEXT,DESCRIPTION TEXT,OVERVIEW TEXT,GENRE TEXT,LOCATION TEXT)");
-        db.execSQL( "CREATE TABLE " + TBL_DVD_DETAILS + "(QRCODE TEXT PRIMARY KEY," +
-                "TITLE TEXT,DESCRIPTION TEXT,OVERVIEW TEXT,GENRE TEXT,LOCATION TEXT)");
+        db.execSQL( "CREATE TABLE " + TBL_DVD + "(" + COL_DVD_QRCODE +" TEXT PRIMARY KEY," +
+                COL_DVD_TITLE+" TEXT,"+COL_DVD_DESCRIPTION+" TEXT,"+COL_DVD_OVERVIEW+" TEXT)");
+        db.execSQL( "CREATE TABLE " + TBL_KEYWORDS + "("+COL_KEYWORD+" TEXT PRIMARY KEY)");
+        db.execSQL( "CREATE TABLE " + TBL_CREDITS + "("+COL_NAME + " TEXT PRIMARY KEY)");
+        db.execSQL( "CREATE TABLE " + TBL_GENRE + "("+COL_GENRE+" TEXT PRIMARY KEY," +
+                COL_DESCRIPTION+" TEXT)");
+        db.execSQL( "CREATE TABLE " + TBL_LOCATION + "("+COL_LOCATION + " TEXT PRIMARY KEY)");
+        //Bridge tables init
+        db.execSQL( "CREATE TABLE " + TBL_DVD_KEYWORDS + "(ID INTEGER PRIMARY KEY AUTOINCREMENT," +
+                        COL_DVD_QRCODE+" TEXT,"+COL_KEYWORD+" TEXT,"+
+                "FOREIGN KEY("+COL_DVD_QRCODE+") REFERENCES " + TBL_DVD+"("+COL_DVD_QRCODE+"),"+
+                "FOREIGN KEY("+COL_KEYWORD+") REFERENCES " + TBL_KEYWORDS+"("+COL_KEYWORD+")"+ ")");
+        db.execSQL( "CREATE TABLE " + TBL_DVD_CREDITS + "(ID INTEGER PRIMARY KEY AUTOINCREMENT," +
+                COL_DVD_QRCODE+" TEXT,"+COL_NAME+" TEXT,"+
+                "FOREIGN KEY("+COL_DVD_QRCODE+") REFERENCES " + TBL_DVD+"("+COL_DVD_QRCODE+"),"+
+                "FOREIGN KEY("+COL_NAME+") REFERENCES " + TBL_CREDITS+ "("+COL_NAME+")"+ ")");
+        db.execSQL( "CREATE TABLE " + TBL_DVD_GENRE + "(ID INTEGER PRIMARY KEY AUTOINCREMENT," +
+                COL_DVD_QRCODE+" TEXT,"+COL_GENRE+" TEXT,"+
+                "FOREIGN KEY("+COL_DVD_QRCODE+") REFERENCES " + TBL_DVD+"("+COL_DVD_QRCODE+"),"+
+                "FOREIGN KEY("+COL_GENRE+") REFERENCES " + TBL_GENRE+ "("+COL_GENRE+")"+ ")");
+        db.execSQL( "CREATE TABLE " + TBL_DVD_LOCATION + "(ID INTEGER PRIMARY KEY AUTOINCREMENT," +
+                COL_DVD_QRCODE+" TEXT,"+COL_LOCATION+" TEXT,"+
+                "FOREIGN KEY("+COL_DVD_QRCODE+") REFERENCES " + TBL_DVD+"("+COL_DVD_QRCODE+"),"+
+                "FOREIGN KEY("+COL_LOCATION+") REFERENCES "+TBL_LOCATION+"("+COL_LOCATION+")"+")");
+
     }
 
+    /**
+     * Called whenever the database must be upgraded, causing all tables to be dropped
+     * Allowing a new database to be initialized
+     * @param db the database reference to be upgraded
+     * @param i optional param
+     * @param i1 optional param
+     */
     @Override
     public void onUpgrade(SQLiteDatabase db, int i, int i1) {
-        db.execSQL( "DROP TABLE IF EXISTS " + TBL_DVD_MAIN);
+        db.execSQL( "DROP TABLE IF EXISTS " + TBL_DVD);
+        db.execSQL( "DROP TABLE IF EXISTS " + TBL_CREDITS);
+        db.execSQL( "DROP TABLE IF EXISTS " + TBL_KEYWORDS);
+        db.execSQL( "DROP TABLE IF EXISTS " + TBL_LOCATION);
+        db.execSQL( "DROP TABLE IF EXISTS " + TBL_GENRE);
+        db.execSQL( "DROP TABLE IF EXISTS " + TBL_DVD_CREDITS);
+        db.execSQL( "DROP TABLE IF EXISTS " + TBL_DVD_KEYWORDS);
+        db.execSQL( "DROP TABLE IF EXISTS " + TBL_DVD_LOCATION);
+        db.execSQL( "DROP TABLE IF EXISTS " + TBL_DVD_GENRE);
+
         onCreate(db);
     }
 
@@ -70,7 +122,7 @@ class DatabaseHelper extends SQLiteOpenHelper {
         cv.put(COL_DVD_QRCODE,qrcode);
 
         try {
-            long result = db.insertOrThrow(TBL_DVD_MAIN, null, cv);
+            long result = db.insertOrThrow(TBL_DVD, null, cv);
             return result != -1;
         }
         catch (SQLiteConstraintException e){
@@ -84,7 +136,7 @@ class DatabaseHelper extends SQLiteOpenHelper {
      */
     public Cursor getData(){
         SQLiteDatabase db = this.getWritableDatabase();
-        Cursor result = db.rawQuery("SELECT * FROM "+ TBL_DVD_MAIN,null);
+        Cursor result = db.rawQuery("SELECT * FROM "+ TBL_DVD,null);
         return result;
     }
 
@@ -108,7 +160,7 @@ class DatabaseHelper extends SQLiteOpenHelper {
             boolean suc = file.createNewFile();
             CsvWriter csvWrite = new CsvWriter(new FileWriter(file));
             Cursor curCSV = db.rawQuery("SELECT "+COL_DVD_QRCODE+","+COL_DVD_TITLE+","+
-                    COL_DVD_LOCATION + " FROM "+ TBL_DVD_MAIN,null);
+                    COL_DVD_LOCATION + " FROM "+ TBL_DVD,null);
             csvWrite.writeNext(curCSV.getColumnNames());
             while(curCSV.moveToNext())
             {
@@ -136,7 +188,7 @@ class DatabaseHelper extends SQLiteOpenHelper {
         cv.put("qrcode",response.getUpc());
         cv.put("title",response.getTitle());
         cv.put("description",response.getDescription());
-        db.update(TBL_DVD_MAIN,cv,"qrcode IS ?", new String[]{response.getUpc()});
+        db.update(TBL_DVD,cv,"qrcode IS ?", new String[]{response.getUpc()});
     }
 
     void updateDvd(Credits credits) {
@@ -145,7 +197,7 @@ class DatabaseHelper extends SQLiteOpenHelper {
         //cv.put("qrcode",details.getUpc());
         //cv.put("title",details.getTitle());
         //cv.put("description",details.getDescription());
-        //db.update(TBL_DVD_MAIN,cv,"qrcode IS ?", new String[]{details.getUpc()});
+        //db.update(TBL_DVD,cv,"qrcode IS ?", new String[]{details.getUpc()});
     }
 
 
