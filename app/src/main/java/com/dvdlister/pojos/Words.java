4b3801d6834deb7c09db1c9feb2bc8f6a202fb73
jplabadie <jhl42@nau.edu>
2017-10-06 22:42:07 -0700

    Refactoring to match Keywords changes

--- app/src/main/java/com/dvdlister/pojos/Words.java
+++ app/src/main/java/com/dvdlister/pojos/Words.java
@@ -5,4 +5,33 @@ package com.dvdlister.pojos;
  */
 
 public class Words {
+    private String id;
+
+    private String name;
+
+    public String getId ()
+    {
+        return id;
+    }
+
+    public void setId (String id)
+    {
+        this.id = id;
+    }
+
+    public String getName ()
+    {
+        return name;
+    }
+
+    public void setName (String name)
+    {
+        this.name = name;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "ClassPojo [id = "+id+", name = "+name+"]";
+    }
 }
