3288371662976858a085483604bfbc2e2989df73
jplabadie <jhl42@nau.edu>
2017-10-07 06:16:19 -0700

    Began work on fragment controller

--- app/src/main/java/com/dvdlister/ScanCompleteFragment.java
+++ app/src/main/java/com/dvdlister/ScanCompleteFragment.java
@@ -1,8 +1,24 @@
 package com.dvdlister;
 
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 /**
  * Created by Jean-Paul on 10/6/2017.
  */
 
-public class ScanCompleteFragment {
+public class ScanCompleteFragment extends Fragment {
+
+    public ScanCompleteFragment(){
+
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        return rootView;
+    }
 }
