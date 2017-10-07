0eac0fbf703aa82c80a2ef41d8a94639ea1cbd30
jplabadie <jhl42@nau.edu>
2017-10-07 06:13:36 -0700

    Add pojos for omdb api response and ratings

--- app/src/main/java/com/dvdlister/pojos/OmdbRatings.java
+++ app/src/main/java/com/dvdlister/pojos/OmdbRatings.java
@@ -4,5 +4,36 @@ package com.dvdlister.pojos;
  * Created by Jean-Paul on 10/7/2017.
  */
 
-public class OmdbRatings {
+public class OmdbRatings
+{
+    private String Source;
+
+    private String Value;
+
+    public String getSource ()
+    {
+        return Source;
+    }
+
+    public void setSource (String Source)
+    {
+        this.Source = Source;
+    }
+
+    public String getValue ()
+    {
+        return Value;
+    }
+
+    public void setValue (String Value)
+    {
+        this.Value = Value;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "ClassPojo [Source = "+Source+", Value = "+Value+"]";
+    }
 }
+
