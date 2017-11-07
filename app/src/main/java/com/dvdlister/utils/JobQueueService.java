d287af1e93fa9b61c3058dfef1fca81f9cd2b995
jplabadie <jhl42@nau.edu>
2017-11-07 01:27:15 -0700

    Add JobIntentService class for API call handling

--- app/src/main/java/com/dvdlister/utils/JobQueueService.java
+++ app/src/main/java/com/dvdlister/utils/JobQueueService.java
@@ -4,5 +4,65 @@ package com.dvdlister.utils;
  * Created by Jean-Paul on 11/1/2017.
  */
 
-public class JobQueueService {
+import android.content.Context;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.SystemClock;
+import android.support.v4.app.JobIntentService;
+import android.util.Log;
+import android.widget.Toast;
+
+/**
+ * Example implementation of a JobIntentService.
+ */
+public class JobQueueService extends JobIntentService {
+    /**
+     * Unique job ID for this service.
+     */
+    static final int JOB_ID = 1000;
+
+    /**
+     * Convenience method for enqueuing work in to this service.
+     */
+    public static void enqueueWork(Context context, Intent work) {
+        enqueueWork(context, JobQueueService.class, JOB_ID, work);
+    }
+
+    @Override
+    protected void onHandleWork(Intent intent) {
+        // We have received work to do.  The system or framework is already
+        // holding a wake lock for us at this point, so we can just go.
+        Log.i("SimpleJobIntentService", "Executing work: " + intent);
+        String label = intent.getStringExtra("label");
+        if (label == null) {
+            label = intent.toString();
+        }
+        toast("Executing: " + label);
+        for (int i = 0; i < 5; i++) {
+            Log.i("SimpleJobIntentService", "Running service " + (i + 1)
+                    + "/5 @ " + SystemClock.elapsedRealtime());
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+            }
+        }
+        Log.i("SimpleJobIntentService", "Completed service @ " + SystemClock.elapsedRealtime());
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        toast("All work complete");
+    }
+
+    final Handler mHandler = new Handler();
+
+    // Helper for showing tests
+    void toast(final CharSequence text) {
+        mHandler.post(new Runnable() {
+            @Override public void run() {
+                Toast.makeText( JobQueueService.this, text, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
 }
