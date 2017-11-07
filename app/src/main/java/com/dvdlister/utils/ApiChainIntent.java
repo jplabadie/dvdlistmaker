a03db2c7b946e98cca1812b0b304a05b268b3b33
jplabadie <jhl42@nau.edu>
2017-11-07 01:23:45 -0700

    Add Intent to contain API calls for jobscheduling

--- app/src/main/java/com/dvdlister/utils/ApiChainIntent.java
+++ app/src/main/java/com/dvdlister/utils/ApiChainIntent.java
@@ -1,8 +1,10 @@
 package com.dvdlister.utils;
 
+import android.content.Intent;
+
 /**
  * Created by Jean-Paul on 11/1/2017.
  */
 
-public class ApiChainIntent {
+public class ApiChainIntent extends Intent {
 }
